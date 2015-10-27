6d28dc2f65d323dd1f556df31f9f98c628eb0930
Johnny Lai <johnny.lai@me.com>
2015-10-26 17:38:36 -0700

    Added boot.mk and app.go

---
+++ app.go
@@ -0,0 +1,108 @@
+package bedrock
+
+import (
+  "errors"
+  "fmt"
+  "github.com/codegangsta/cli"
+  "github.com/gin-gonic/gin"
+  "gopkg.in/yaml.v1"
+  "io/ioutil"
+  "os"
+  "log"
+)
+
+type AppServicer interface {
+  Migrate(cfg map[interface{}]interface{}) error
+  Build(cfg map[interface{}]interface{}, r *gin.Engine) error
+}
+
+func GetConfig(yamlPath string) (map[interface{}]interface{}, error) {
+  config := make(map[interface{}]interface{})
+
+  if _, err := os.Stat(yamlPath); err != nil {
+    return config, errors.New("config path not valid")
+  }
+
+  ymlData, err := ioutil.ReadFile(yamlPath)
+  if err != nil {
+    return config, err
+  }
+
+  err = yaml.Unmarshal([]byte(ymlData), &config)
+
+  /*config.SvcHost = os.Expand(config.SvcHost, os.Getenv)
+  config.DbUser = os.Expand(config.DbUser, os.Getenv)
+  config.DbPassword = os.Expand(config.DbPassword, os.Getenv)
+  config.DbHost = os.Expand(config.DbHost, os.Getenv)
+  config.DbName = os.Expand(config.DbName, os.Getenv)
+  */
+
+  return config, err
+}
+
+func NewApp(svc AppServicer) *cli.App {
+  app := cli.NewApp()
+  
+  app.Flags = []cli.Flag{
+    cli.StringFlag{
+      Name:  "config, c",
+      Value: "config.yaml",
+      Usage: "config file to use",
+    },
+  }
+
+  app.Commands = []cli.Command{
+    {
+      Name:  "env",
+      Usage: "Print the configurations",
+      Action: func(c *cli.Context) {
+        cfg, err := GetConfig(c.GlobalString("config"))
+        if err != nil {
+          log.Fatal(err)
+          return
+        }
+
+        d, err := yaml.Marshal(&cfg)
+        if err != nil {
+          log.Fatalf("error: %v", err)
+        }
+        fmt.Printf(string(d))
+      },
+    },
+    {
+      Name:  "server",
+      Usage: "Run the http server",
+      Action: func(c *cli.Context) {
+        cfg, err := GetConfig(c.GlobalString("config"))
+        if err != nil {
+          log.Fatal(err)
+          return
+        }
+
+        r := gin.Default()
+        if err = svc.Build(cfg, r); err != nil {
+          log.Fatal(err)
+        }
+
+        r.Run(cfg["SvcHost"].(string))
+      },
+    },
+    {
+      Name:  "migratedb",
+      Usage: "Perform database migrations",
+      Action: func(c *cli.Context) {
+        cfg, err := GetConfig(c.GlobalString("config"))
+        if err != nil {
+          log.Fatal(err)
+          return
+        }
+
+        if err = svc.Migrate(cfg); err != nil {
+          log.Fatal(err)
+        }
+      },
+    },
+  }
+
+  return app
+}
---
+++ boot.mk
@@ -0,0 +1,126 @@
+GLIDE = $(GOPATH)/bin/glide
+
+MAJOR_VERSION ?= 0
+MINOR_VERSION ?= 0
+BUILD_NUMBER ?= 0
+COMMIT ?= $(shell git log --pretty=format:'%h' -n 1)
+VERSION = $(MAJOR_VERSION).$(MINOR_VERSION).$(BUILD_NUMBER)
+
+DOCKER_DEVIMAGE ?= johnnylai/golang-dev
+
+# These are local paths
+SRCROOT ?= $(realpath .)
+BUILD_ROOT ?= $(SRCROOT)
+DOCKER_ROOT ?= $(SRCROOT)/docker
+TEST_CONFIG_YML ?= $(SRCROOT)/config/test.yml
+PRODUCT_PATH = tmp/dist/$(APP_NAME)
+
+# These are paths used in the docker image
+SRCROOT_D = /go/src/$(APP_NAME)
+BUILD_ROOT_D = $(SRCROOT_D)/tmp/dist
+TEST_CONFIG_YML_D = $(SRCROOT_D)/config/production.yml
+
+#
+SERVER ?= $(shell kubectl get svc go-service-basic -o json | jq -r '.spec.clusterIP')
+PORT ?= $(shell kubectl get svc go-service-basic -o json | jq '.spec.ports[0].targetPort')
+
+default: build
+
+clean:
+	rm -f $(BUILD_ROOT)/$(APP_NAME)
+	rm -rf tmp
+
+build: deps
+	GO15VENDOREXPERIMENT=1 go build \
+		-o $(BUILD_ROOT)/$(APP_NAME) \
+		-ldflags "-X main.version=$(VERSION) -X main.commit=$(COMMIT)" \
+		$(APP_NAME).go
+
+deps: $(GLIDE) $(BUILD_ROOT)
+	if [ ! -d vendor ]; then $(GLIDE) update; fi
+
+migrate:
+	./cmd/server/server --config config.yaml migratedb
+
+utest: deps
+	TEST_CONFIG_YML=$(TEST_CONFIG_YML) GO15VENDOREXPERIMENT=1 go test $(APP_GO_PACKAGES)
+
+itest:
+	TEST_HOST="http://$(SERVER):$(PORT)" go test $(APP_NAME)/itest
+
+bench:
+	TEST_HOST="http://$(SERVER):$(PORT)" go test -bench=. $(APP_NAME)/itest
+
+itest-env-restart:
+	cd itest/env && make restart
+
+fmt:
+	GO15VENDOREXPERIMENT=1 go fmt $(APP_GO_PACKAGES)
+
+devconsole:
+	docker run --rm \
+	           --net=host \
+	           -v $(SRCROOT):$(SRCROOT_D) \
+	           -w $(SRCROOT_D) \
+	           -e GO15VENDOREXPERIMENT=1 \
+	           -it \
+	           $(DOCKER_DEVIMAGE)
+
+dist: image-dist image-testdb
+
+distbuild: clean build
+	chown -R $(UID):$(GID) $(SRCROOT)
+
+distitest:
+	docker run --rm --net=host \
+	           -v $(SRCROOT):$(SRCROOT_D) \
+ 	           -w $(SRCROOT_D) \
+	           $(DOCKER_DEVIMAGE) \
+	           make itest
+
+distibench:
+	docker run --rm --net=host \
+	           -v $(SRCROOT):$(SRCROOT_D) \
+ 	           -w $(SRCROOT_D)/itest \
+	           $(DOCKER_DEVIMAGE) \
+	           make bench
+
+distutest:
+	-docker rm -f $(APP_NAME)-testdb
+	docker run -d --name $(APP_NAME)-testdb $(APP_DOCKER_LABEL)-testdb
+	docker run --rm \
+	           --link $(APP_NAME)-testdb:$(APP_NAME)-db \
+	           -v $(SRCROOT):$(SRCROOT_D) \
+	           -w $(SRCROOT_D) \
+	           -e DB_ENV_MYSQL_ROOT_PASSWORD=whatever \
+	           -e TEST_CONFIG_YML=$(TEST_CONFIG_YML_D) \
+	           $(DOCKER_DEVIMAGE) \
+	           make utest
+
+deploy: distutest dist itest-env-restart distitest
+	docker push $(APP_DOCKER_LABEL)
+
+.PHONY: build clean default deploy deps dist distbuild fmt migrate itest utest
+
+image-testdb:
+	docker build -f $(DOCKER_ROOT)/testdb/Dockerfile -t $(APP_DOCKER_LABEL)-testdb .
+
+image-dist: $(PRODUCT_PATH)
+	docker build -f $(DOCKER_ROOT)/dist/Dockerfile -t $(APP_DOCKER_LABEL) .
+
+$(GLIDE):
+	go get github.com/Masterminds/glide
+
+$(BUILD_ROOT):
+	mkdir -p $@
+
+$(PRODUCT_PATH): $(wildcard *.go)
+	docker run --rm \
+	           -v $(SRCROOT):$(SRCROOT_D) \
+	           -w $(SRCROOT_D) \
+	           -e BUILD_ROOT=$(BUILD_ROOT_D) \
+	           -e BUILD_NUMBER=$(BUILD_NUMBER) \
+	           -e UID=`id -u` \
+	           -e GID=`id -g` \
+	           $(DOCKER_DEVIMAGE) \
+	           make distbuild
