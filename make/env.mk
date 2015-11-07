feedddc691ec88cf5fbae740677933b6ef9b7802
Johnny Lai <johnny.lai@me.com>
2015-11-06 20:31:53 -0800

    Clean up env.mk

--- make/env.mk
+++ make/env.mk
@@ -1,14 +1,22 @@
-GLIDE = $(GOPATH)/bin/glide
-BEDROCK = $(BEDROCK_ROOT)/bedrock
+# These are local paths
+SRCROOT ?= $(abspath .)
+DOCKER_ROOT ?= $(SRCROOT)/docker
+TEST_CONFIG_YML ?= $(SRCROOT)/config/test.yml
+PRODUCT_PATH = tmp/dist/$(APP_NAME)
 
+# These are paths used in the docker image
+SRCROOT_D = /go/src/$(APP_NAME)
+BUILD_ROOT_D = $(SRCROOT_D)/tmp/dist
+TEST_CONFIG_YML_D = $(SRCROOT_D)/config/production.yml
+
+# The current version
 MAJOR_VERSION ?= 0
 MINOR_VERSION ?= 0
 BUILD_NUMBER ?= 0
 COMMIT ?= $(shell git log --pretty=format:'%h' -n 1)
 VERSION = $(MAJOR_VERSION).$(MINOR_VERSION).$(BUILD_NUMBER)
 
-KUBERNETES_CONFIG ?= $(BEDROCK_ROOT)/make/kubernetes.config.default
-
+# Application settings
 APP_NAME ?= unset
 APP_DOCKER_LABEL ?= unset
 APP_GO_LINKING ?= dynamic
@@ -16,6 +24,15 @@ APP_GO_SOURCES ?= $(APP_NAME).go
 APP_DOCKER_PUSH ?= yes
 APP_ITEST_ENV_ROOT ?= $(BEDROCK_ROOT)/make/itest/env
 
+# Docker Labels
+APP_DOCKER_LABEL_VERSION = $(APP_DOCKER_LABEL):$(MAJOR_VERSION).$(MINOR_VERSION)
+APP_DOCKER_LABEL_COMMIT = $(APP_DOCKER_LABEL):$(COMMIT)
+
+TESTDB_DOCKER_LABEL ?= $(APP_DOCKER_LABEL)-testdb
+TESTDB_DOCKER_LABEL_VERSION = $(TESTDB_DOCKER_LABEL):$(MAJOR_VERSION).$(MINOR_VERSION)
+TESTDB_DOCKER_LABEL_COMMIT = $(TESTDB_DOCKER_LABEL):$(COMMIT)
+
+# Docker commands
 DOCKER_DEVIMAGE ?= johnnylai/bedrock-dev:17cbf55
 DOCKER_DEV_UID ?= $(shell which docker-machine &> /dev/null || id -u)
 DOCKER_DEV_GID ?= $(shell which docker-machine &> /dev/null || id -g)
@@ -31,26 +48,12 @@ else
 	DOCKER_PUSH ?= docker push
 endif
 
-# Docker Labels
-APP_DOCKER_LABEL_VERSION = $(APP_DOCKER_LABEL):$(MAJOR_VERSION).$(MINOR_VERSION)
-APP_DOCKER_LABEL_COMMIT = $(APP_DOCKER_LABEL):$(COMMIT)
+# Kubernetes config
+KUBERNETES_CONFIG ?= $(BEDROCK_ROOT)/make/kubernetes.config.default
 
-TESTDB_DOCKER_LABEL ?= $(APP_DOCKER_LABEL)-testdb
-TESTDB_DOCKER_LABEL_VERSION = $(TESTDB_DOCKER_LABEL):$(MAJOR_VERSION).$(MINOR_VERSION)
-TESTDB_DOCKER_LABEL_COMMIT = $(TESTDB_DOCKER_LABEL):$(COMMIT)
-
-# These are local paths
-SRCROOT ?= $(abspath .)
-
-DOCKER_ROOT ?= $(SRCROOT)/docker
-TEST_CONFIG_YML ?= $(SRCROOT)/config/test.yml
-PRODUCT_PATH = tmp/dist/$(APP_NAME)
-
-# These are paths used in the docker image
-SRCROOT_D = /go/src/$(APP_NAME)
-BUILD_ROOT_D = $(SRCROOT_D)/tmp/dist
-TEST_CONFIG_YML_D = $(SRCROOT_D)/config/production.yml
-
+# Executables
+GLIDE = $(GOPATH)/bin/glide
+BEDROCK = $(SRCROOT)/bedrock
 
 deps: $(GLIDE) $(BUILD_ROOT)
 	if [ ! -d vendor/github.com/gin-gonic/gin ]; then $(GLIDE) update; fi
