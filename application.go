627fd29ef1f3e8988daeff6a6ba35a5b3769263d
Johnny Lai <johnny.lai@me.com>
2016-10-20 13:13:10 -0700

    Add support for configuring the logs through the config file

--- application.go
+++ application.go
@@ -3,9 +3,12 @@ package bedrock
 import (
 	"bytes"
 	"errors"
+	"fmt"
 	log "github.com/Sirupsen/logrus"
+	logrus_syslog "github.com/Sirupsen/logrus/hooks/syslog"
 	"github.com/codegangsta/cli"
 	"gopkg.in/yaml.v2"
+	"log/syslog"
 	"os"
 	"path"
 	"text/template"
@@ -15,6 +18,17 @@ import (
 type Application struct {
 	cli.App
 	ConfigBytes []byte
+	Config      Config
+}
+
+type Config struct {
+	Log LogConfig `yaml:"log"`
+}
+
+type LogConfig struct {
+	Level      string
+	Formatter  string `yaml:"formatter"`
+	SyslogName string `yaml:"syslog_name"`
 }
 
 // Reads the specified config file. Note that bedrock.Application will process
@@ -82,18 +96,53 @@ func (app *Application) BindConfigAt(config interface{}, key string) error {
 }
 
 func (app *Application) InitFromCliContext(c *cli.Context) error {
+	var err error
+
 	// Set config
 	config := c.GlobalString("config")
-	if err := app.ReadConfigFile(config); err != nil {
+	if err = app.ReadConfigFile(config); err != nil {
+		return err
+	}
+
+	// Read config
+	if err = app.BindConfig(&app.Config); err != nil {
 		return err
 	}
 
 	// Set log level
+	var level = log.InfoLevel
 	if c.GlobalBool("debug") {
-		log.SetLevel(log.DebugLevel)
+		level = log.DebugLevel
+	} else if app.Config.Log.Level != "" {
+		level, err = log.ParseLevel(app.Config.Log.Level)
+		if err != nil {
+			log.Warnf("Failed to parse log level: %v", err)
+		}
+	}
+	log.SetLevel(level)
+
+	// Set log formatter
+	var formatter = "text"
+	if app.Config.Log.Formatter != "" {
+		formatter = app.Config.Log.Formatter
+	}
+	logFormatter, err := app.parseLogFormatter(formatter)
+	if err != nil {
+		return err
+	}
+	log.SetFormatter(logFormatter)
+
+	// Add DebugLoggerHook if we are in debug mode
+	if log.GetLevel() == log.DebugLevel {
 		log.AddHook(new(DebugLoggerHook))
+	}
+
+	// Add syslog
+	syslog_hook, err := logrus_syslog.NewSyslogHook("", "", syslog.LOG_INFO, app.Config.Log.SyslogName)
+	if err == nil {
+		log.AddHook(syslog_hook)
 	} else {
-		log.SetLevel(log.InfoLevel)
+		log.Warnf("Failed to use syslog: %v", err)
 	}
 
 	return nil
@@ -113,6 +162,17 @@ func (app *Application) initCli() {
 		},
 	}
 	app.App.Commands = []cli.Command{}
+}
+
+func (app *Application) parseLogFormatter(formatter string) (log.Formatter, error) {
+	switch formatter {
+	case "json":
+		return &log.JSONFormatter{}, nil
+	case "text":
+		return &log.TextFormatter{}, nil
+	default:
+		return nil, fmt.Errorf("Unknown log formatter %s requested", formatter)
+	}
 }
 
 // Creates a new application
